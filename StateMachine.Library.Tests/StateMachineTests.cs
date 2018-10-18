2549e42aeff7bb917132631a47facf3570e9f82d
Mongooz <Mongooz@users.noreply.github.com>
2018-10-18 21:07:16 +1100

    Update to .Net core and remove obsolete configuration projects

---
+++ StateMachine.Library.Tests/StateMachineTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StateMachine.Library.Tests
+{
+    [TestClass]
+    public class StateMachineTests
+    {
+        enum States { Locked, Unlocked, Opened }
+
+        [TestMethod]
+        public void TestSetup()
+        {
+            IStateMachine<States> stateMachine = new StateMachine<States>(States.Locked);
+            stateMachine.Setup()
+                .When(States.Locked).Allow(States.Unlocked).OnTransition(TurnKey).Then(Unlock)
+                .When(States.Unlocked).Allow(States.Locked).OnTransition(TurnKey).Then(Lock)
+                .When(States.Unlocked).Allow(States.Opened).OnTransition(Open)
+                .When(States.Opened).Allow(States.Unlocked).OnTransition(Close);
+
+            Assert.IsTrue(stateMachine.TryTransition(States.Unlocked));
+            Assert.IsTrue(stateMachine.TryTransition(States.Locked));
+            Assert.IsFalse(stateMachine.TryTransition(States.Opened));
+            Assert.IsTrue(stateMachine.TryTransition(States.Unlocked));
+            Assert.IsTrue(stateMachine.TryTransition(States.Opened));
+            Assert.IsFalse(stateMachine.TryTransition(States.Locked));
+            Assert.IsTrue(stateMachine.TryTransition(States.Unlocked));
+            Assert.IsTrue(stateMachine.TryTransition(States.Locked));
+        }
+
+        private void TurnKey()
+        {
+            Console.WriteLine("Key Turned");
+        }
+
+        private void Unlock()
+        {
+            Console.WriteLine("Unlocked");
+        }
+
+        private void Lock()
+        {
+            Console.WriteLine("Locked");
+        }
+
+        private void Open()
+        {
+            Console.WriteLine("Opened");
+        }
+
+        private void Close()
+        {
+            Console.WriteLine("Closed");
+        }
+    }
+}
